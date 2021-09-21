e70037787cc1eac7548df9a948035d88b9f97d01
Jesus Mª Rico Gonzalez <52455810+jmrg-link@users.noreply.github.com>
2021-09-21 19:30:36 +0200

    añadiendo ejemplos de monohilo y variables de Entorno

--- conceptos/varEntorno.js
+++ conceptos/varEntorno.js
@@ -6,6 +6,7 @@
 
 //+ Por buenas prácticas heredadas de Linux las variables de entorno que vengan desde fuera (process.env.VARIABLE ) se ponen en mayúscula y se separan mediante guion bajo en vez de espacio.
 
+//config
 let nomae ="Jrico" //Esta variable la declaré aquí y puedo llamarla aquí.
 
 let nombre = process.env.NOMBRE || 'not_name'; 
@@ -15,10 +16,11 @@ const apiKey = process.env.APIKEY || 'HF33o9oERVERVEEEEs';
 console.log('Hola '+ nombre);
 console.log('Mi web es '+ web);
 
+
 // Me retornara { Hola Sin nombre, mi web es notengo web} ya que tiene  la expresión OR || me asigna estas variables por defecto
 
 // Para que las variables de entorno me tomen valores puedo hacer varias cosas:
 
 //+  Desde la terminal asignarle las variables antes del codigo:
 
-//< NOMBRE:"Alejandro" MI_WEB"123QWERTY" APIKEY=10937472 node conceptos/entorno.js >
+//< NOMBRE:"Jesus" MI_WEB"123QWERTY" APIKEY=10937472 node conceptos/entorno.js >
