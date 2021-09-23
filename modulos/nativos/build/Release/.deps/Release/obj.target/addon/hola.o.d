9a9abb8bba3746db6b0fa0e4f9d378348d701eb6
Jesus Mª Rico Gonzalez <52455810+jmrg-link@users.noreply.github.com>
2021-09-23 23:39:15 +0200

    añadiendo modulos/nativos/node-gyp

---
+++ modulos/nativos/build/Release/.deps/Release/obj.target/addon/hola.o.d
@@ -0,0 +1,19 @@
+cmd_Release/obj.target/addon/hola.o := c++ -o Release/obj.target/addon/hola.o ../hola.cc '-DNODE_GYP_MODULE_NAME=addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/Users/jrico/Library/Caches/node-gyp/14.17.6/include/node -I/Users/jrico/Library/Caches/node-gyp/14.17.6/src -I/Users/jrico/Library/Caches/node-gyp/14.17.6/deps/openssl/config -I/Users/jrico/Library/Caches/node-gyp/14.17.6/deps/openssl/openssl/include -I/Users/jrico/Library/Caches/node-gyp/14.17.6/deps/uv/include -I/Users/jrico/Library/Caches/node-gyp/14.17.6/deps/zlib -I/Users/jrico/Library/Caches/node-gyp/14.17.6/deps/v8/include  -O3 -gdwarf-2 -mmacosx-version-min=10.13 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++1y -stdlib=libc++ -fno-rtti -fno-exceptions -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/addon/hola.o.d.raw   -c
+Release/obj.target/addon/hola.o: ../hola.cc \
+  /Users/jrico/Library/Caches/node-gyp/14.17.6/include/node/node.h \
+  /Users/jrico/Library/Caches/node-gyp/14.17.6/include/node/v8.h \
+  /Users/jrico/Library/Caches/node-gyp/14.17.6/include/node/cppgc/common.h \
+  /Users/jrico/Library/Caches/node-gyp/14.17.6/include/node/v8config.h \
+  /Users/jrico/Library/Caches/node-gyp/14.17.6/include/node/v8-internal.h \
+  /Users/jrico/Library/Caches/node-gyp/14.17.6/include/node/v8-version.h \
+  /Users/jrico/Library/Caches/node-gyp/14.17.6/include/node/v8-platform.h \
+  /Users/jrico/Library/Caches/node-gyp/14.17.6/include/node/node_version.h
+../hola.cc:
+/Users/jrico/Library/Caches/node-gyp/14.17.6/include/node/node.h:
+/Users/jrico/Library/Caches/node-gyp/14.17.6/include/node/v8.h:
+/Users/jrico/Library/Caches/node-gyp/14.17.6/include/node/cppgc/common.h:
+/Users/jrico/Library/Caches/node-gyp/14.17.6/include/node/v8config.h:
+/Users/jrico/Library/Caches/node-gyp/14.17.6/include/node/v8-internal.h:
+/Users/jrico/Library/Caches/node-gyp/14.17.6/include/node/v8-version.h:
+/Users/jrico/Library/Caches/node-gyp/14.17.6/include/node/v8-platform.h:
+/Users/jrico/Library/Caches/node-gyp/14.17.6/include/node/node_version.h:
